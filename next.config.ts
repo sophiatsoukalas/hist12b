13f963d3339b8b0001664d91ebe0caa3167cb2f5
Sophia Tsoukalas <sophiatsoukalas@g.ucla.edu>
2026-02-23 16:24:39 -0800

    added to next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: '/hist12b',
+  assetPrefix: '/hist12b/',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
